6ea7a6fb51738ff21f92a3297cdfed9795845c4f
hass <auttenberg@icloud.com>
2024-02-25 20:49:28 +0300

    Refactor for variant

--- vue/src/components/App.vue
+++ vue/src/components/App.vue
@@ -1,69 +1,91 @@
 <template>
-  <div class="main-block"> <!-- BEM style for block html & css -->
-    hello
-    <div class="main-block__element"> <!-- BEM style for element html & css -->
-      {{ value1 }}  <!-- spaces and new row for output -->
-    </div>
-    <div class="main-block__element main-block__element--alert"> <!-- BEM style for modificator html & css -->
-      {{ value1 }}
-    </div>
-    <div
-        class="main-block__element"
-        :title="value2"
-    > <!-- for 3 and more attrs or less attrs with long length, every attr with new row, end of start tag on new own row -->
-      {{ value1 }}
-    </div>
-    <OtherBtn :title="value2"> <!-- props and dynamic attrs transfer with : -->
-      {{ value1 }}
-    </OtherBtn>
-    <OtherBtn @change="(v) => valueHandler(v)"> <!-- event's handler need to set up as arrow functions -->
-      {{ value1 }}
-    </OtherBtn>
+  <div>
+    <RouterView />
+    <ModalContainer />
   </div>
 </template>
 
 <script>
-import { mapGetters, mapMutations, mapActions } from 'vuex'
-import OtherBtn from '../OtherBtn.vue'
+import ModalContainer from "@/components/parts/ModalContainer";
 
 export default {
-  name: 'CustomBtn', // component's name contain 2 or more words in CamelCase
   components: {
-    OtherBtn
-  },
-  data () { // component's data is function with returned object
-    return {
-      value1: 1,
-      value2: 'second' // no comma
-    }
-  },
-  computed: {
-    ...mapGetters('user', [ // for use getters of store
-      'getUser'
-    ]),
-    values () { // style functions for computed
-      return this.value + this.value
-    },
-    customValue: { // style for computable values with custom get/set methods
-      get () {
-        return this.value
-      },
-      set (v) {
-        this.value = v
-      }
-    }
-  },
-  methods: {
-    ...mapMutations('user', [ // for use mutations of store
-      'setUser'
-    ]),
-    ...mapActions('user', [ // for use actions of store
-      'loadUser'
-    ]),
-    valueHandler () { // style functions for methods // v, t = 10
-      this.value1 += 1
-      this.value2 += '-'
-    }
+    ModalContainer
   }
 }
 </script>
+
+<style lang="less">
+@import url('https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap');
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: @cBaseTwo;
+}
+
+a {
+  text-decoration: none;
+}
+
+section {
+  background-color: @cBaseOne;
+  margin-bottom: 20px;
+  border-radius: 2px;
+  box-sizing: border-box;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+h1, h2, h3, h4, h5 {
+  font-family: @ffOne;
+  color: @cBaseThree;
+  margin: 0;
+}
+
+h2 {
+  font-size: 32px;
+}
+
+.p-16 {
+  padding: 16px;
+}
+
+.d-flex {
+  display: flex;
+}
+
+.rcms {
+
+  &-divider {
+
+    &-h {
+      width: 100%;
+      height: @sizeBorderDefault;
+      background-color: @cBaseTwo;
+    }
+
+    &-v {
+
+    }
+  }
+
+  &-loading {
+    position: relative;
+
+    &::after {
+      content: '';
+      position: absolute;
+      left: 0;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      z-index: 1;
+      background: rgba(100, 100, 100, 0.5);
+      cursor: wait;
+    }
+  }
+}
+</style>
