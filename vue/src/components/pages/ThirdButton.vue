139e8dcefd72bca0c2e0f98af0bbfdfd3e923f68
tat0e <tat0elen@gmail.com>
2024-02-26 16:19:35 +0300

    Изменения 1.4

--- vue/src/components/pages/ThirdButton.vue
+++ vue/src/components/pages/ThirdButton.vue
@@ -4,24 +4,24 @@
       <div>
         <div class="button-grid">
           <div class="button-grid__column">
-            <ComponentBtn type="warning">WARNING</ComponentBtn>
-            <ComponentBtn type="positive">POSITIVE</ComponentBtn>
-            <ComponentBtn type="attention">ATTENTION</ComponentBtn>
+            <ComponentBtn :style="{'background-color':'#ea3d2f'}">WARNING</ComponentBtn>
+            <ComponentBtn  :style="{'background-color':'#30a74f'}">POSITIVE</ComponentBtn>
+            <ComponentBtn  :style="{'background-color':'#f3aa18'}">ATTENTION</ComponentBtn>
           </div>
           <div class="button-grid__column">
-            <ComponentBtn type="smooth_red">SMOOTH</ComponentBtn>
-            <ComponentBtn type="smooth_green">SMOOTH</ComponentBtn>
-            <ComponentBtn type="smooth_orange">SMOOTH</ComponentBtn>
+            <ComponentBtn :style="{'background-color':'#fde4e1', color:'#ea3d2f'}">SMOOTH</ComponentBtn>
+            <ComponentBtn :style="{'background-color':'#dcf7e3', color:' #30a74f'}">SMOOTH</ComponentBtn>
+            <ComponentBtn :style="{'background-color':'#fdf3d7', color:' #f3aa18'}">SMOOTH</ComponentBtn>
           </div>
           <div class="button-grid__column">
-            <ComponentBtn type="ghost_red">GHOST</ComponentBtn>
-            <ComponentBtn type="ghost_green">GHOST</ComponentBtn>
-            <ComponentBtn type="ghost_orange">GHOST</ComponentBtn>
+            <ComponentBtn :style="{border:'1px solid #ea3d2f', color:'#ea3d2f'}">GHOST</ComponentBtn>
+            <ComponentBtn :style="{border:'1px solid #30a74f', color:'#30a74f'}">GHOST</ComponentBtn>
+            <ComponentBtn :style="{border:'1px solid #f3aa18', color:'#f3aa18'}">GHOST</ComponentBtn>
           </div>
           <div class="button-grid__column">
-            <ComponentBtn type="raised_red">RAISED</ComponentBtn>
-            <ComponentBtn type="raised_green">RAISED</ComponentBtn>
-            <ComponentBtn type="raised_orange">RAISED</ComponentBtn>
+            <ComponentBtn :style="{color:'#ea3d2f', 'box-shadow':' 0px 3px 5px -3px rgba(0, 0, 0, 0.3)'}">RAISED</ComponentBtn>
+            <ComponentBtn :style="{color:'#30a74f', 'box-shadow':' 0px 3px 5px -3px rgba(0, 0, 0, 0.3)'}">RAISED</ComponentBtn>
+            <ComponentBtn :style="{color:'#f3aa18', 'box-shadow':' 0px 3px 5px -3px rgba(0, 0, 0, 0.3)'}">RAISED</ComponentBtn>
           </div>
         </div>
       </div>
--- vue/src/components/ui/buttons/ComponentBtn.vue
+++ vue/src/components/ui/buttons/ComponentBtn.vue
@@ -1,33 +1,53 @@
 <template>
-  <button :class = "['simple-button', type]">
-    <slot>
-    </slot>
-  </button>
+    <button class="sample-button" :style="buttonStyle">
+        <slot>
+        </slot>
+    </button>
 </template>
 
 <script>
   export default {
     name: 'ComponentBtn',
     props: {
-      type: {
+      backgroundColor: {
         type: String,
-        default: "attention",
-        validator: function (value) {
-          return ["warning", "positive", "attention",
-                  "smooth_red", "smooth_green", "smooth_orange",
-                  "ghost_red", "ghost_green", "ghost_orange",
-                  "raised_red", "raised_green", "raised_orange"].includes(value);
+        default: 'white'
+      },
+      textColor: {
+        type: String,
+        default: 'white'
+      },
+      border: {
+        type: String,
+        default: 'none'
+      },
+      shadow: {
+        type: String,
+        default: 'none'
+      }
+    },
+    computed: {
+      buttonStyle() {
+        return {
+          '--btn-background-color': this.backgroundColor,
+          '--btn-text-color': this.textColor,
+          '--btn-border': this.border,
+          '--btn-shadow': this.shadow,
         }
       }
-    }
+    }    
   }
 </script>
 
-<style lang="less">
-  .simple-button{
+<style scoped lang="less">
+  .sample-button {
     width: 110px;
     height: 35px;
     border-radius: 4px;
+    background: var(--btn-background-color);
+    color: var(--btn-text-color);
+    border: var(--btn-border);
+    box-shadow: var(--btn-);
     padding: 0.5% 1%;
     box-sizing: border-box;
     margin: 3% 0.75%;
@@ -36,63 +56,6 @@
     color: white;
     cursor: pointer;
     display: block;
-  }
-  .warning {
-    background-color: #ea3d2f;
-    border: none;
-  }
-  .positive {
-    background-color: #30a74f;
-    border: none;
-  }
-  .attention {
-    background-color: #f3aa18;
-    border: none;
-  }
-  .smooth_red {
-    background-color: #fde4e1;
-    color: #ea3d2f;
-    border: none;
-  }
-  .smooth_green {
-    background-color: #dcf7e3;
-    color: #30a74f;
-    border: none;
-  }
-  .smooth_orange {
-    background-color: #fdf3d7;
-    color: #f3aa18;
-    border: none;
-  }
-  .ghost_red {
-    color: #ea3d2f;
-    border-width: 1px;
-    border-color: #ea3d2f;
-  }
-  .ghost_green {
-    color: #30a74f;
-    border-width: 1px;
-    border-color: #30a74f;
-  }
-  .ghost_orange {
-    color: #f3aa18;
-    border-width: 1px;
-    border-color: #f3aa18;
-  }
-  .raised_red {
-    color: #ea3d2f;
-    box-shadow: 0px 3px 5px -3px rgba(0, 0, 0, 0.3);
-    border: none;
-  }
-  .raised_green {
-    color: #30a74f;
-    box-shadow: 0px 3px 5px -3px rgba(0, 0, 0, 0.3);
-    border: none;
-  }
-  .raised_orange {
-    color: #f3aa18;
-    box-shadow: 0px 3px 5px -3px rgba(0, 0, 0, 0.3);
-    border: none;
   }
 
 </style>
