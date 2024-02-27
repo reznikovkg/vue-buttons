65c3037b12f7333039638cf2f4b27e0534556013
tat0e <tat0elen@gmail.com>
2024-02-27 23:05:48 +0300

    Изменения 1.8

--- vue/src/components/pages/ThirdButton.vue
+++ vue/src/components/pages/ThirdButton.vue
@@ -1,55 +1,53 @@
 <template>
   <PageLayout>
-    <section class="p-16">
-      <div>
-        <div class="button-grid">
-          <div class="button-grid__column">
-            <ComponentBtn type="warning">
-              WARNING
-            </ComponentBtn>
-            <ComponentBtn type="positive">
-              POSITIVE
-            </ComponentBtn>
-            <ComponentBtn type="attention">
-              ATTENTION
-            </ComponentBtn>
-          </div>
-          <div class="button-grid__column">
-            <ComponentBtn type="smooth_red">
-              SMOOTH
-            </ComponentBtn>
-            <ComponentBtn type="smooth_green">
-              SMOOTH
-            </ComponentBtn>
-            <ComponentBtn type="smooth_orange">
-              SMOOTH
-            </ComponentBtn>
-          </div>
-          <div class="button-grid__column">
-            <ComponentBtn type="ghost_red">
-              GHOST
-            </ComponentBtn>
-            <ComponentBtn type="ghost_green">
-              GHOST
-            </ComponentBtn>
-            <ComponentBtn type="ghost_orange">
-              GHOST
-            </ComponentBtn>
-          </div>
-          <div class="button-grid__column">
-            <ComponentBtn type="raised_red">
-              RAISED
-            </ComponentBtn>
-            <ComponentBtn type="raised_green">
-              RAISED
-            </ComponentBtn>
-            <ComponentBtn type="raised_orange">
-              RAISED
-            </ComponentBtn>
-          </div>
+    <div>
+      <div class="button-grid">
+        <div class="button-grid__column">
+          <ComponentBtn type="warning">
+            WARNING
+          </ComponentBtn>
+          <ComponentBtn type="positive">
+            POSITIVE
+          </ComponentBtn>
+          <ComponentBtn type="attention">
+            ATTENTION
+          </ComponentBtn>
+        </div>
+        <div class="button-grid__column">
+          <ComponentBtn type="smooth_red">
+            SMOOTH
+          </ComponentBtn>
+          <ComponentBtn type="smooth_green">
+            SMOOTH
+          </ComponentBtn>
+          <ComponentBtn type="smooth_orange">
+            SMOOTH
+          </ComponentBtn>
+        </div>
+        <div class="button-grid__column">
+          <ComponentBtn type="ghost_red">
+            GHOST
+          </ComponentBtn>
+          <ComponentBtn type="ghost_green">
+            GHOST
+          </ComponentBtn>
+          <ComponentBtn type="ghost_orange">
+            GHOST
+          </ComponentBtn>
+        </div>
+        <div class="button-grid__column">
+          <ComponentBtn type="raised_red">
+            RAISED
+          </ComponentBtn>
+          <ComponentBtn type="raised_green">
+            RAISED
+          </ComponentBtn>
+          <ComponentBtn type="raised_orange">
+            RAISED
+          </ComponentBtn>
         </div>
       </div>
-    </section>
+    </div>
   </PageLayout>
 </template>
 
