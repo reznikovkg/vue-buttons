16d2ac837b31d5ed7d033037ebcd2514218f8aa9
tat0e <tat0elen@gmail.com>
2024-02-27 22:52:55 +0300

    Изменение 1.8

--- vue/src/components/pages/ThirdButton.vue
+++ vue/src/components/pages/ThirdButton.vue
@@ -4,24 +4,48 @@
       <div>
         <div class="button-grid">
           <div class="button-grid__column">
-            <ComponentBtn type="warning">WARNING</ComponentBtn>
-            <ComponentBtn type="positive">POSITIVE</ComponentBtn>
-            <ComponentBtn type="attention">ATTENTION</ComponentBtn>
+            <ComponentBtn type="warning">
+              WARNING
+            </ComponentBtn>
+            <ComponentBtn type="positive">
+              POSITIVE
+            </ComponentBtn>
+            <ComponentBtn type="attention">
+              ATTENTION
+            </ComponentBtn>
           </div>
           <div class="button-grid__column">
-            <ComponentBtn type="smooth_red">SMOOTH</ComponentBtn>
-            <ComponentBtn type="smooth_green">SMOOTH</ComponentBtn>
-            <ComponentBtn type="smooth_orange">SMOOTH</ComponentBtn>
+            <ComponentBtn type="smooth_red">
+              SMOOTH
+            </ComponentBtn>
+            <ComponentBtn type="smooth_green">
+              SMOOTH
+            </ComponentBtn>
+            <ComponentBtn type="smooth_orange">
+              SMOOTH
+            </ComponentBtn>
           </div>
           <div class="button-grid__column">
-            <ComponentBtn type="ghost_red">GHOST</ComponentBtn>
-            <ComponentBtn type="ghost_green">GHOST</ComponentBtn>
-            <ComponentBtn type="ghost_orange">GHOST</ComponentBtn>
+            <ComponentBtn type="ghost_red">
+              GHOST
+            </ComponentBtn>
+            <ComponentBtn type="ghost_green">
+              GHOST
+            </ComponentBtn>
+            <ComponentBtn type="ghost_orange">
+              GHOST
+            </ComponentBtn>
           </div>
           <div class="button-grid__column">
-            <ComponentBtn type="raised_red">RAISED</ComponentBtn>
-            <ComponentBtn type="raised_green">RAISED</ComponentBtn>
-            <ComponentBtn type="raised_orange">RAISED</ComponentBtn>
+            <ComponentBtn type="raised_red">
+              RAISED
+            </ComponentBtn>
+            <ComponentBtn type="raised_green">
+              RAISED
+            </ComponentBtn>
+            <ComponentBtn type="raised_orange">
+              RAISED
+            </ComponentBtn>
           </div>
         </div>
       </div>
@@ -43,15 +67,15 @@ export default {
 </script>
 
 <style lang="less">
-  .button-grid {
-    display: flex;
+.button-grid {
+  display: flex;
 
-    &__column {
-      flex: 1;
-      max-width: 150px;
-      display: flex;
-      flex-direction: column;
-      gap: 30px;
-    }
+  &__column {
+    flex: 1;
+    max-width: 150px;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
   }
+}
 </style>
--- vue/src/components/ui/buttons/ComponentBtn.vue
+++ vue/src/components/ui/buttons/ComponentBtn.vue
@@ -1,6 +1,7 @@
 <template>
   <button class="sample-button" :class="buttonClass">
     <slot>
+      WARNING
     </slot>
   </button>
 </template>
@@ -18,83 +19,83 @@ export default {
     buttonClass () {
       return `sample-button--${this.type}`;
     }
-  }    
+  }
 }
 </script>
 
 <style scoped lang="less">
-  .sample-button {
-    width: 110px;
-    height: 35px;
-    border-radius: 4px;
-    background-color: white;
-    font-family: Calibri;
-    font-size: 18px;
-    color: white;
-    cursor: pointer;
-    border: none;
+.sample-button {
+  width: 110px;
+  height: 35px;
+  border-radius: 4px;
+  background-color: white;
+  font-family: Calibri;
+  font-size: 18px;
+  color: white;
+  cursor: pointer;
+  border: none;
 
-    &--warning{
-      background-color: #ea3d2f;
-    }
+  &--warning {
+    background-color: #ea3d2f;
+  }
 
-    &--positive {
-      background-color: #37be5b;
-    }
+  &--positive {
+    background-color: #37be5b;
+  }
 
-    &--attention {
-      background-color: #f3aa18;
-    }     
+  &--attention {
+    background-color: #f3aa18;
+  }
 
-    &--smooth_red {
-      background-color: lighten(#ea3d2f, 40%);
-      color: #ea3d2f;
-    }
+  &--smooth_red {
+    background-color: lighten(#ea3d2f, 40%);
+    color: #ea3d2f;
+  }
 
-    &--smooth_green {
-      background-color: lighten(#37be5b, 40%);
-      color: #37be5b;
-    }
+  &--smooth_green {
+    background-color: lighten(#37be5b, 40%);
+    color: #37be5b;
+  }
 
-    &--smooth_orange {
-      background-color: lighten(#f3aa18, 40%);
-      color: #f3aa18;
-    }
+  &--smooth_orange {
+    background-color: lighten(#f3aa18, 40%);
+    color: #f3aa18;
+  }
 
-    &--ghost_red {
-      color: #ea3d2f;
-      border-width: 1px;
-      border-style: solid;
-      border-color: #ea3d2f;
-    }
+  &--ghost_red {
+    color: #ea3d2f;
+    border-width: 1px;
+    border-style: solid;
+    border-color: #ea3d2f;
+  }
 
-    &--ghost_green {
-      color: #37be5b;
-      border-width: 1px;
-      border-style: solid;
-      border-color: #37be5b;
-    }
+  &--ghost_green {
+    color: #37be5b;
+    border-width: 1px;
+    border-style: solid;
+    border-color: #37be5b;
+  }
 
-    &--ghost_orange {
-      color: #f3aa18;
-      border-width: 1px;
-      border-style: solid;
-      border-color: #f3aa18;
-    }
+  &--ghost_orange {
+    color: #f3aa18;
+    border-width: 1px;
+    border-style: solid;
+    border-color: #f3aa18;
+  }
 
-    &--raised_red {
-      color: #ea3d2f;
-      box-shadow: 0px 3px 5px -3px rgba(0, 0, 0, 0.3);
-    }
+  &--raised_red {
+    color: #ea3d2f;
+    box-shadow: 0px 3px 5px -3px rgba(0, 0, 0, 0.3);
+  }
 
-    &--raised_green {
-      color: #37be5b;
-      box-shadow: 0px 3px 5px -3px rgba(0, 0, 0, 0.3);
-    }
+  &--raised_green {
+    color: #37be5b;
+    box-shadow: 0px 3px 5px -3px rgba(0, 0, 0, 0.3);
+  }
 
-    &--raised_orange {
-      color: #f3aa18;
-      box-shadow: 0px 3px 5px -3px rgba(0, 0, 0, 0.3);
-    }
-  } 
+  &--raised_orange {
+    color: #f3aa18;
+    box-shadow: 0px 3px 5px -3px rgba(0, 0, 0, 0.3);
+  }
+}
 </style>
