c5e6798b10af149e9919391ca109da4a22342238
Demiurrg <forge0047@gmail.com>
2024-02-28 01:34:59 +0300

    Minor fixes: parameters formatted as '--color', proper indents

--- vue/src/components/ui/buttons/FirstButton.vue
+++ vue/src/components/ui/buttons/FirstButton.vue
@@ -15,65 +15,65 @@ export default {
   },
   computed: {
     getColor() {
-      return `first-button-${this.color}`;
+      return `first-button--${this.color}`;
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-  .first-button {
-    border: none;
-    color: white;
-    padding: 10px 35px;
-    font-size: 18px;
+.first-button {
+  border: none;
+  color: white;
+  padding: 10px 35px;
+  font-size: 18px;
+  border-radius: 6px;
+  position: relative;
+
+  &--green {
+    background: #5cbb98;
+  }
+
+  &--white {
+    background: #e6ebed;
+    color: #6e8a95;
+    border: 1px solid #d5dce0;
+  }
+
+  &--red {
+    background: #e0886a;
+  }
+
+  &:after {
+    content:'';
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    right: 0;
+    bottom: 0;
     border-radius: 6px;
-    position: relative;
+  }
 
-    &-green {
-      background: #5cbb98;
-    }
+  &:hover {
+    box-shadow: 0 -3px 0 rgba(0, 0, 0, 0.2) inset;
+    cursor: pointer;
+  }
 
-    &-white {
-      background: #e6ebed;
-      color: #6e8a95;
-      border: 1px solid #d5dce0;
-    }
+  &:active {
+    box-shadow: none;
 
-    &-red {
-      background: #e0886a;
-    }
-
-    &:after {
-      content:'';
-      height: 100%;
-      width: 100%;
-      position: absolute;
-      right: 0;
-      bottom: 0;
-      border-radius: 6px;
-    }
-
-    &:hover {
-      box-shadow: 0 -3px 0 rgba(0, 0, 0, 0.2) inset;
-      cursor: pointer;
-    }
-
-    &:active {
-      box-shadow: none;
-
-      &::after {
-        background-color: rgba(255, 255, 255, 0.2);
-      }
-    }
-
-    &:disabled {
-      box-shadow: none;
-      cursor: default;
-
-      &::after {
-        background-color: rgba(255, 255, 255, 0.4);
-      }
+    &::after {
+      background-color: rgba(255, 255, 255, 0.2);
     }
   }
+
+  &:disabled {
+    box-shadow: none;
+    cursor: default;
+
+    &::after {
+      background-color: rgba(255, 255, 255, 0.4);
+    }
+  }
+}
 </style>
