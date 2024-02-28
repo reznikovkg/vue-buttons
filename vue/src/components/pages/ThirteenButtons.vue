630f19b3e53e21894db31bef122bb33671c14f6b
flezzzer <141737861+flezzzer@users.noreply.github.com>
2024-02-28 12:06:44 +0300

    message) 2.0

--- vue/src/components/pages/ThirteenButtons.vue
+++ vue/src/components/pages/ThirteenButtons.vue
@@ -1,21 +1,32 @@
 <template>
-  <PageLayout>
-    <ThirteenthButton setting-button="black"/>
-    <ThirteenthButton setting-button="blue"/>
-    <ThirteenthButton setting-button="green"/>
-    <ThirteenthButton setting-button="red"/>
-  </PageLayout>
+  <div class="wrapper">
+    <ThirteenthButton />
+    <ThirteenthButton type="blue"/>
+    <ThirteenthButton type="green"/>
+    <ThirteenthButton type="red"/>
+  </div>
 </template>
 
 <script>
-import ThirteenthButton from "@/components/ui/buttons/ThirteenthButton.vue"
-import PageLayout from "@/components/parts/PageLayout.vue";
+import ThirteenthButton from "@/components/ui/buttons/ThirteenthButton";
 
 export default {
-  name: 'ThirteenthButton',
+  name: 'ThirteenthButtonPage',
   components: {
-    PageLayout,
     ThirteenthButton
   }
 }
 </script>
+
+<style scoped lang="less">
+.wrapper {
+  max-width: 60%;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  grid-gap: 20px;
+  margin: 0 auto;
+  padding: 40px 0;
+}
+</style>
