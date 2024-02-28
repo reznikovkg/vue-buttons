60736172f31ac9123942c2a078fa88db9e16025b
tat0e <tat0elen@gmail.com>
2024-02-28 12:04:51 +0300

    Изменения 1.9

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomePage from '../components/pages/Home.vue'
-import ThirdButton from '../components/pages/ThirdButton.vue'
 import { RouteNames } from './routes'
+import ButtonSixthPage from "@/components/pages/ButtonSixthPage.vue"
+import FourthButtonPage from "@/components/pages/FourthButtons.vue";
+import TwelfthButtonPage from '../components/pages/TwelfthButtonPage.vue'
+import ThirdButton from '../components/pages/ThirdButton.vue'
 
 Vue.use(VueRouter)
 
@@ -11,6 +14,21 @@ const routes = [
     path: '/',
     name: RouteNames.HOME,
     component: HomePage
+  },
+  {
+    path: '/fourth_button',
+    name: RouteNames.FOURTH_BUTTON,
+    component: FourthButtonPage
+  },
+  {
+    path: '/button_sixth',
+    name: RouteNames.BUTTON_SIXTH,
+    component: ButtonSixthPage
+  },
+  {
+    path: '/twelfth_button',
+    name: RouteNames.TWELFTH_BUTTON,
+    component: TwelfthButtonPage
   },
   {
     path: '/third-button',
