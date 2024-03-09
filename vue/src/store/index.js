6f227bd4aa5035f0b005cd10b64ebae1563308bb
VsevolodMu <Vsevolod-2003@yandex.ru>
2024-03-09 23:31:20 +0300

    Some punctuation changes

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -2,12 +2,11 @@ import modals from './modals'
 import sixthButton from './sixthButton';
 
 export default {
-    namespaced: true,
-    state: {},
-    getters: {},
-    mutations: {},
-    modules: {
-        modals,
-        sixthButton
-    }
+  namespaced: true, state: {},
+  getters: {},
+  mutations: {},
+  modules: {
+    modals,
+    sixthButton
+  }
 }
--- vue/src/store/sixthButton.js
+++ vue/src/store/sixthButton.js
@@ -1,29 +1,29 @@
 import modals from './modals'
 
 export default {
-    namespaced: true,
-    state: {
-        buttonCounts: {
-            button1: 0,
-            button2: 0,
-            button3: 0,
-            button4: 0,
-        }
-    },
-    getters: {
-        getCounts: (state) => state.buttonCounts
-    },
-    mutations: {
-        incrementCount: (state, name) => {
-            ++state.buttonCounts[name]
-        }
-    },
-    actions: {
-        incrementCount: ({ commit }, name) => {
-            commit('incrementCount', name)
-        }
-    },
-    modules: {
-        modals
+  namespaced: true,
+  state: {
+    buttonCounts: {
+      button1: 0,
+      button2: 0,
+      button3: 0,
+      button4: 0,
     }
+  },
+  getters: {
+    getCounts: (state) => state.buttonCounts
+  },
+  mutations: {
+    incrementCount: (state, name) => {
+      ++state.buttonCounts[name]
+    }
+  },
+  actions: {
+    incrementCount: ({ commit }, name) => {
+      commit('incrementCount', name)
+    }
+  },
+  modules: {
+    modals
+  }
 }
