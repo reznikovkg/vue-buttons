15bc93c77ff545b4d3049c842ecea1b2633001ed
DanielZiborov <ziborovdanila5@gmail.com>
2024-03-10 15:34:13 +0300

    new_commit

--- vue/src/components/pages/SeventhButtonPage.vue
+++ vue/src/components/pages/SeventhButtonPage.vue
@@ -1,24 +1,26 @@
 <template>
   <div>
-    <SeventhButton/>
-    <SeventhButton type="primary">
-      PRIMARY
+    <SeventhButton @click.native="increment('button1')"/>
+    <span>({{ getCounts.button1 }})</span>
+
+    <SeventhButton type="primary" @click.native="increment('button2')">
+      PRIMARY <span>({{ getCounts.button2 }})</span>
     </SeventhButton>
 
-    <SeventhButton type="info">
-      INFO
+    <SeventhButton type="info" @click.native="increment('button3')">
+      INFO <span>({{ getCounts.button3 }})</span>
     </SeventhButton>
 
-    <SeventhButton type="success">
-      SUCCESS
+    <SeventhButton type="success" @click.native="increment('button4')">
+      SUCCESS <span>({{ getCounts.button4 }})</span>
     </SeventhButton>
 
-    <SeventhButton type="warning">
-      WARNING
+    <SeventhButton type="warning" @click.native="increment('button5')">
+      WARNING <span>({{ getCounts.button5 }})</span>
     </SeventhButton>
 
-    <SeventhButton type="danger">
-      DANGER
+    <SeventhButton type="danger" @click.native="increment('button6')">
+      DANGER <span>({{ getCounts.button6 }})</span>
     </SeventhButton>
 
   </div>
@@ -26,10 +28,24 @@
 
 <script>
 import SeventhButton from "@/components/ui/buttons/SeventhButton";
+import {mapActions, mapGetters} from "vuex";
 
 export default {
   components: {
     SeventhButton
+  },
+   computed: {
+    ...mapGetters('seventhButton', [
+      'getCounts',
+    ]),
+  },
+  methods: {
+    ...mapActions('seventhButton', [
+      'incrementCount',
+    ]),
+    increment(index) {
+      this.incrementCount(index)
+    }
   }
 }
 </script>
---
+++ vue/src/store/seventhButton.js
@@ -0,0 +1,31 @@
+import modals from './modals'
+
+export default {
+  namespaced: true,
+  state: {
+    buttonCounts: {
+      button1: 0,
+      button2: 0,
+      button3: 0,
+      button4: 0,
+      button5: 0,
+      button6: 0,
+    }
+  },
+  getters: {
+    getCounts: (state) => state.buttonCounts
+  },
+  mutations: {
+    incrementCount: (state, name) => {
+      state.buttonCounts[name]++
+    }
+  },
+  actions: {
+    incrementCount: ({ commit }, name) => {
+      commit('incrementCount', name)
+    }
+  },
+  modules: {
+    modals
+  }
+}
