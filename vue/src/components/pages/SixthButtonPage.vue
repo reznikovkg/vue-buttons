af247072d98e29d0469e9f565e4c812f9d1c1783
VsevolodMu <Vsevolod-2003@yandex.ru>
2024-03-10 13:42:26 +0300

    Some changes

--- vue/src/components/pages/SixthButtonPage.vue
+++ vue/src/components/pages/SixthButtonPage.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <SixthButton @click.native="increment('button1')">
-      HALLO <span>{{ getCounts.button1 }}</span>
+      HALLO <span>({{ getCounts.button1 }})</span>
     </SixthButton>
     <SixthButton type="red" @click.native="increment('button2')">
-      HALLO <span>{{ getCounts.button2 }}</span>
+      HALLO <span>({{ getCounts.button2 }})</span>
     </SixthButton>
     <SixthButton type="blue" @click.native="increment('button3')">
-      MARHABAN <span>{{ getCounts.button3 }}</span>
+      MARHABAN <span>({{ getCounts.button3 }})</span>
     </SixthButton>
     <SixthButton type="purple" @click.native="increment('button4')">
-      NI HAO <span>{{ getCounts.button4 }}</span>
+      NI HAO <span>({{ getCounts.button4 }})</span>
     </SixthButton>
   </div>
 </template>
