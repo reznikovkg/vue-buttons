05db265c517fc1d6cd453934c508037ec6377b2b
VsevolodMu <Vsevolod-2003@yandex.ru>
2024-03-14 11:55:01 +0300

    Добавил имя компонента

--- vue/src/components/pages/SixthButtonPage.vue
+++ vue/src/components/pages/SixthButtonPage.vue
@@ -1,26 +1,30 @@
 <template>
-  <div>
-    <SixthButton @click="() => incr('btn1')">
-      HALLO <span>({{ getCounts.btn1 }})</span>
-    </SixthButton>
-    <SixthButton type="red" @click="() => incr('btn2')">
-      HALLO <span>({{ getCounts.btn2 }})</span>
-    </SixthButton>
-    <SixthButton type="blue" @click="() => incr('btn3')">
-      MARHABAN <span>({{ getCounts.btn3 }})</span>
-    </SixthButton>
-    <SixthButton type="purple" @click="() => incr('btn4')">
-      NI HAO <span>({{ getCounts.btn4 }})</span>
-    </SixthButton>
-  </div>
+  <page-layout>
+    <div>
+      <SixthButton @click="() => incr('btn1')">
+        HALLO <span>({{ getCounts.btn1 }})</span>
+      </SixthButton>
+      <SixthButton type="red" @click="() => incr('btn2')">
+        HALLO <span>({{ getCounts.btn2 }})</span>
+      </SixthButton>
+      <SixthButton type="blue" @click="() => incr('btn3')">
+        MARHABAN <span>({{ getCounts.btn3 }})</span>
+      </SixthButton>
+      <SixthButton type="purple" @click="() => incr('btn4')">
+        NI HAO <span>({{ getCounts.btn4 }})</span>
+      </SixthButton>
+    </div>
+  </page-layout>
 </template>
 
 <script>
 import SixthButton from "@/components/ui/buttons/SixthButton.vue";
 import {mapActions, mapGetters} from "vuex";
+import PageLayout from "@/components/parts/PageLayout.vue";
 
 export default {
   components: {
+    PageLayout,
     SixthButton
   },
   computed: {
