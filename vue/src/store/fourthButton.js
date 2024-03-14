5f7cf2bf36a1cda5b98f07f0ed668e38ffdaccc3
pavel-chernyshev <pasha7w@gmail.com>
2024-03-14 11:40:30 +0300

    выровнял

--- vue/src/store/fourthButton.js
+++ vue/src/store/fourthButton.js
@@ -2,12 +2,12 @@ export default {
   namespaced: true,
   state: {
     counts: {
-        b1: 0,
-        b2: 0,
-        b3: 0,
-        b4: 0,
-        b5: 0,
-        b6: 0
+      b1: 0,
+      b2: 0,
+      b3: 0,
+      b4: 0,
+      b5: 0,
+      b6: 0
     }
   },
   getters: {
