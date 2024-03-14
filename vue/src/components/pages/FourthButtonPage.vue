cb6fcf1ab4e9f4f11d70c9a14b0efff4bdaef0bb
pavel-chernyshev <pasha7w@gmail.com>
2024-03-14 08:28:10 +0300

    minor changes

--- vue/src/components/pages/FourthButtonPage.vue
+++ vue/src/components/pages/FourthButtonPage.vue
@@ -1,27 +1,52 @@
 <template>
   <PageLayout>
-    <FourthButton @click.native="() => increment('b1')" bg-start="khaki" bg-end="darkorange" box-shadow-color="orange">
+    <FourthButton
+      bg-start="khaki"
+      bg-end="darkorange"
+      box-shadow-color="orange"
+      @click="() => increment('b1')"
+    >
       Subscribe (<span>{{ getButtonCounts.b1 }}</span>)
     </FourthButton>
 
-    <FourthButton @click.native="() => increment('b2')" bg-start="orange" bg-end="deeppink" box-shadow-color="hotpink">
+    <FourthButton
+      bg-start="orange"
+      bg-end="deeppink"
+      box-shadow-color="hotpink"
+      @click="() => increment('b2')"
+    >
       Click here (<span>{{ getButtonCounts.b2 }}</span>)
     </FourthButton>
 
-    <FourthButton @click.native="() => increment('b3')" bg-start="deeppink" bg-end="red" box-shadow-color="mediumvioletred">
+    <FourthButton
+      bg-start="deeppink"
+      bg-end="red"
+      box-shadow-color="mediumvioletred"
+      @click="() => increment('b3')"
+    >
       Download (<span>{{ getButtonCounts.b3 }}</span>)
     </FourthButton>
 
     <!--default-->
-    <FourthButton @click.native="() => increment('b4')">
+    <FourthButton @click="() => increment('b4')">
       Play now (<span>{{ getButtonCounts.b4 }}</span>)
     </FourthButton>
 
-    <FourthButton @click.native="() => increment('b5')" bg-start="deeppink" bg-end="mediumpurple" box-shadow-color="purple">
+    <FourthButton
+      bg-start="deeppink"
+      bg-end="mediumpurple"
+      box-shadow-color="purple"
+      @click="() => increment('b5')"
+    >
       Explore (<span>{{ getButtonCounts.b5 }}</span>)
     </FourthButton>
 
-    <FourthButton @click.native="() => increment('b6')" bg-start="mediumpurple" bg-end="rebeccapurple" box-shadow-color="purple">
+    <FourthButton
+      bg-start="mediumpurple"
+      bg-end="rebeccapurple"
+      box-shadow-color="purple"
+      @click="() => increment('b6')"
+    >
       Read more (<span>{{ getButtonCounts.b6 }}</span>)
     </FourthButton>
   </PageLayout>
--- vue/src/store/fourthButton.js
+++ vue/src/store/fourthButton.js
@@ -7,7 +7,7 @@ export default {
         b3: 0,
         b4: 0,
         b5: 0,
-        b6: 0,
+        b6: 0
     }
   },
   getters: {
