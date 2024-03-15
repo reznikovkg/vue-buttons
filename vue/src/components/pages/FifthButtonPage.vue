113cbe836e74ffcbb67c94d732135a9b7bb4142d
ivanov <maxim2003.maks@mail.ru>
2024-03-15 19:46:23 +0300

    Выровнены отступы

--- vue/src/components/pages/FifthButtonPage.vue
+++ vue/src/components/pages/FifthButtonPage.vue
@@ -8,27 +8,27 @@
       ({{ getCount.btn5_1 }})
     </FifthButton>
     <FifthButton
-        type="green"
-        btnName="btn5_2"
-        @click="() => increase('btn5_2')">
+      type="green"
+      btnName="btn5_2"
+      @click="() => increase('btn5_2')">
       ({{ getCount.btn5_2 }})
     </FifthButton>
     <FifthButton
-        type="red"
-        btnName="btn5_3"
-        @click="() => increase('btn5_3')">
+      type="red"
+      btnName="btn5_3"
+      @click="() => increase('btn5_3')">
       ({{ getCount.btn5_3 }})
     </FifthButton>
     <FifthButton
-        type="orange"
-        btnName="btn5_4"
-        @click="() => increase('btn5_4')">
+      type="orange"
+      btnName="btn5_4"
+      @click="() => increase('btn5_4')">
       ({{ getCount.btn5_4 }})
     </FifthButton>
     <FifthButton
-        type="black"
-        btnName="btn5_5"
-        @click="() => increase('btn5_5')">
+      type="black"
+      btnName="btn5_5"
+      @click="() => increase('btn5_5')">
       ({{ getCount.btn5_5 }})
     </FifthButton>
   </div>
