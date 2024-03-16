cc63555d68ed1754701d9e14a116fad5b55f6a9a
Шуленин Илья <131488642+IlyushaShulenin@users.noreply.github.com>
2024-03-17 02:44:36 +0300

    Кнопка 11 (#52)

---
+++ vue/src/components/pages/EleventhPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <PageLayout>
+    <div>
+      <EleventhButton type="successful">SUCCESSFUL</EleventhButton>
+      <EleventhButton type="info">INFO</EleventhButton>
+      <EleventhButton type="warning">WARNING</EleventhButton>
+      <EleventhButton type="danger">DANGER</EleventhButton>
+    </div>
+  </PageLayout>
+</template>
+
+<script>
+import PageLayout from '../parts/PageLayout';
+import EleventhButton from "@/components/ui/buttons/EleventhButton.vue";
+
+export default {
+  components: {
+    PageLayout,
+    EleventhButton
+  },
+}
+</script>
+
+<style>
+.title {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  grid-gap: 20px;
+}
+</style>
---
+++ vue/src/components/ui/buttons/EleventhButton.vue
@@ -0,0 +1,107 @@
+<template>
+  <button class="eleventh-btn" :class="className">
+    <slot>
+      content
+    </slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "EleventhPage",
+  props: {
+    type: String,
+  },
+  computed: {
+    className () {
+      return `eleventh-btn__${this.type}`;
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+  .eleventh-btn {
+    font-family: "Montserrat Alternates", sans-serif;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 10px;
+    border: 2px solid;
+
+    &__successful {
+      background-color: #04AA6D;
+      color: white;
+      border: 2px solid;
+
+      &:active {
+        color: white;
+        background-color: #04AA6D;
+        border-radius: 50px;
+      }
+
+      &:hover {
+        background-color: white;
+        color: #04AA6D;
+        border: 2px solid #04AA6D;
+      }
+    }
+
+    &__info {
+      background-color: #f0c502;
+      color: white;
+      border: 2px solid #f0c502;
+
+      &:active {
+        color: white;
+        background-color: #f0c502;
+        border-radius: 50px;
+      }
+
+      &:hover {
+        background-color: white;
+        color: #f0c502;
+        border: 2px solid #f0c502;
+      }
+    }
+
+    &__warning {
+      background-color: #e17b01;
+      color: white;
+      border: 2px solid #e17b01;
+
+      &:active {
+        color: white;
+        background-color: #e17b01;
+        border-radius: 50px;
+      }
+
+      &:hover {
+        background-color: white;
+        color: #e17b01;
+        border: 2px solid #e17b01;
+      }
+    }
+
+    &__danger {
+      background-color: #b8154c;
+      color: white;
+      border: 2px solid #b8154c;
+
+      &:active {
+        color: white;
+        background-color: #b8154c;
+        border-radius: 50px;
+      }
+
+      &:hover {
+        background-color: white;
+        color: #b8154c;
+        border: 2px solid #b8154c;
+      }
+    }
+  }
+</style>
