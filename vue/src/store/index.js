00932faa58f7de6b0ad98dd81a63d0d8ed0ace90
Artem K <146346948+TrubArt@users.noreply.github.com>
2024-03-17 02:42:31 +0300

    Кнопка 2#3 (#48)
    * test1
    
    * Кнопка #2
    
    * rubbish update + page update
    
    * add button types, -style
    
    * bug fix
    
    * return elses code and add right code
    
    * fix span

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -2,6 +2,7 @@ import modals from './modals'
 import ThirteenthButtonStore from "./ThirteenthButtonStore";
 import twelfthButton from './twelfthButton'
 import fourthButton from './fourthButton';
+import secondButton from './secondButton'
 import thirdButton from './thirdButton'
 import sixthButton from './sixthButton'
 import tenthButton from './tenthButton'
@@ -16,6 +17,7 @@ export default {
     modals,
     ThirteenthButtonStore,
     twelfthButton,
+    secondButton,
     thirdButton,
     fourthButton,
     sixthButton,
