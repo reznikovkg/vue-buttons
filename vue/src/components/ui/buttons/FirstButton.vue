9481f4769b34db54f49a7f0251b192e84020fa53
Demiurrg <forge0047@gmail.com>
2024-03-18 21:22:26 +0300

    First button, lab 2: minor fixes

--- vue/src/components/ui/buttons/FirstButton.vue
+++ vue/src/components/ui/buttons/FirstButton.vue
@@ -1,7 +1,9 @@
 <template>
-  <button class="first-button" :class=getColor
+  <button
+    class="first-button"
+    :class=getColor
     @click="() => click()"
-    >
+  >
     <slot/>
   </button>
 </template>
